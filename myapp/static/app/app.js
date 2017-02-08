814cc260f2382fba9a93d44e110b79c58e3a5521
niks0101 <niks9289.bomble@gmail.com>
2017-02-08 21:56:34 +0530

    Update app.js

--- myapp/static/app/app.js
+++ myapp/static/app/app.js
@@ -76,14 +76,14 @@ app.run(function ($rootScope,$location,$cookies ,$http) {
     }
 
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
-
-        $rootScope.activetab=$location.$$path;
-        // redirect to login page if not logged in and trying to access a restricted page
-        var restrictedPage = $.inArray($location.path(), ['/!/login', '/signup']) === -1;
-        var loggedIn = $rootScope.globals.currentUser;
-        if (restrictedPage && !loggedIn) {
-            $location.path('/login');
-        }
+           $location.path('/signup');
+//         $rootScope.activetab=$location.$$path;
+//         // redirect to login page if not logged in and trying to access a restricted page
+//         var restrictedPage = $.inArray($location.path(), ['/login', '/signup']) === -1;
+//         var loggedIn = $rootScope.globals.currentUser;
+//         if (restrictedPage && !loggedIn) {
+//             $location.path('/login');
+//         }
     });
 
    /* $rootScope.user = {};
